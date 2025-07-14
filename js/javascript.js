1ca74802ff6dff5840a88decf69bba7415cbc694
Cecilia <cecilia.delacamara@gmail.com>
2025-07-14 18:04:59 +0200

    pop up de cookies

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+// Menu desplegable 
+
 let lanzador = "#menu-link";
 let desplegable = ".main-menu";
 let despliegaClase = "dropdown-menu";
@@ -13,3 +15,17 @@ function despliegaMenu(){
 }
 
 nav();
+
+// Cookies 
+
+const popup = document.getElementById("cookiePopup");
+const btn = document.getElementById("acceptCookies");
+
+if (!localStorage.cookiesAccepted){
+    popup.classList.add("show");
+}
+
+btn.addEventListener("click", function(){
+    localStorage.cookiesAccepted = "true";
+    popup.classList.remove("show");
+});
